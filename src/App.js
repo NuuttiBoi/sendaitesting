a9c8d98b30b4d5f7047cb26128dd165d41f3e520
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2023-12-10 18:31:02 +0900

    all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,331 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useReducer, useState} from "react";
+import jsPDF from "jspdf";
+import "./style.css";
+import MakeData from "./makeData";
+import Table from "./Table";
+import { randomColor, shortId } from "./utils";
+import { grey } from "./colors";
+import axios from "axios";
+import work_types_service from "./services/work_types_service";
+import Pdf from "./toPdf";
+import {usePDF} from "react-to-pdf";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add_option_to_column":
+      const optionIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, optionIndex),
+          {
+            ...state.columns[optionIndex],
+            options: [
+              ...state.columns[optionIndex].options,
+              { label: action.option, backgroundColor: action.backgroundColor }
+            ]
+          },
+          ...state.columns.slice(optionIndex + 1, state.columns.length)
+        ]
+      };
+    case "add_row":
+      const newWork = {
+        work1_name: '',
+        work2_name: '',
+        work3_name: '',
+        work4_name:''
+      }
+
+      work_types_service.create(newWork)
+          .then(response => {
+            console.log("success", response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      console.log('saving ', newWork)
+      return {
+        ...state,
+        skipReset: true,
+        data: [...state.data, {}]
+
+      };
+    case "update_column_type":
+      const typeIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+      );
+      switch (action.dataType) {
+        case "number":
+          if (state.columns[typeIndex].dataType === "number") {
+            return state;
+          } else {
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ],
+              data: state.data.map((row) => ({
+                ...row,
+                [action.columnId]: isNaN(row[action.columnId])
+                    ? ""
+                    : Number.parseInt(row[action.columnId])
+              }))
+            };
+          }
+        case "select":
+          if (state.columns[typeIndex].dataType === "select") {
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ],
+              skipReset: true
+            };
+          } else {
+            let options = [];
+            state.data.forEach((row) => {
+              if (row[action.columnId]) {
+                options.push({
+                  label: row[action.columnId],
+                  backgroundColor: randomColor()
+                });
+              }
+            });
+            return {
+              ...state,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                {
+                  ...state.columns[typeIndex],
+                  dataType: action.dataType,
+                  options: [...state.columns[typeIndex].options, ...options]
+                },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ],
+              skipReset: true
+            };
+          }
+        case "text":
+          if (state.columns[typeIndex].dataType === "text") {
+            return state;
+          } else if (state.columns[typeIndex].dataType === "select") {
+            return {
+              ...state,
+              skipReset: true,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ]
+            };
+          } else {
+            return {
+              ...state,
+              skipReset: true,
+              columns: [
+                ...state.columns.slice(0, typeIndex),
+                { ...state.columns[typeIndex], dataType: action.dataType },
+                ...state.columns.slice(typeIndex + 1, state.columns.length)
+              ],
+              data: state.data.map((row) => ({
+                ...row,
+                [action.columnId]: row[action.columnId] + ""
+              }))
+            };
+          }
+        default:
+          return state;
+      }
+    case "update_column_header":
+      const index = state.columns.findIndex(
+          (column) => column.id === action.columnId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, index),
+          { ...state.columns[index], label: action.label },
+          ...state.columns.slice(index + 1, state.columns.length)
+        ]
+      };
+    case "update_cell":
+      return {
+        ...state,
+        skipReset: true,
+        data: state.data.map((row, index) => {
+          if (index === action.rowIndex) {
+            return {
+              ...state.data[action.rowIndex],
+              [action.columnId]: action.value
+            };
+          }
+          return row;
+        })
+      };
+    case "add_column_to_left":
+      const leftIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+      );
+      let leftId = shortId();
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, leftIndex),
+          {
+            id: leftId,
+            label: "Column",
+            accessor: leftId,
+            dataType: "text",
+            created: action.focus && true,
+            options: []
+          },
+          ...state.columns.slice(leftIndex, state.columns.length)
+        ]
+      };
+    case "add_column_to_right":
+      const rightIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+      );
+      const rightId = shortId();
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, rightIndex + 1),
+          {
+            id: rightId,
+            label: "Column",
+            accessor: rightId,
+            dataType: "text",
+            created: action.focus && true,
+            options: []
+          },
+          ...state.columns.slice(rightIndex + 1, state.columns.length)
+        ]
+      };
+    case "delete_column":
+      const deleteIndex = state.columns.findIndex(
+          (column) => column.id === action.columnId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        columns: [
+          ...state.columns.slice(0, deleteIndex),
+          ...state.columns.slice(deleteIndex + 1, state.columns.length)
+        ]
+      };
+    case "delete_row":
+      const deleteRowIndex = state.rows.findIndex(
+          (row) => row.id === action.rowId
+      );
+      return {
+        ...state,
+        skipReset: true,
+        rows: [
+          ...state.rows.slice(0, deleteRowIndex),
+          ...state.rows.slice(deleteRowIndex + 1, state.rows.length)
+        ]
+      };
+    case "enable_reset":
+      return {
+        ...state,
+        skipReset: false
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+
+  const [data, setData] = useState(['']);
+
+  const [work3Name, setWork3Name] = useState(null);
+  const [work1Name, setWork1Name] = useState(null);
+  const [work2Name, setWork2Name] = useState(null);
+
+  const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
+
+  useEffect(() => {
+        axios.get("http://localhost:3001/work_types")
+            .then(response => setData(response.data))
+            .catch(error => console.error(error));
+      }
+  )
+
+
+  const [state, dispatch] = useReducer(reducer, MakeData(10));
+
+  // const [state, dispatch] = data;
+
+  useEffect(() => {
+    dispatch({ type: "enable_reset" });
+  }, [state.columns, state.data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+          style={{
+            width: "100vw",
+            height: "100vh",
+            overflowX: "hidden"
+          }}
+      >
+        <div
+            style={{
+              height: 120,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column"
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h1 style={{ color: grey(800) }}>Create your own work table</h1>
+        </div>
+        <div style={{ overflow: "auto", display: "flex" }}>
+          <div
+              style={{
+                flex: "1 1 auto",
+                padding: "1rem",
+                maxWidth: 1000,
+                marginLeft: "auto",
+                marginRight: "auto"
+              }}
+          >
+            <div>
+              <div ref={targetRef}>
+            <Table
+                columns={state.columns}
+                rows={state.rows}
+                data={data}
+                dispatch={dispatch}
+                skipReset={state.skipReset}
+            />
+                <button className="button" onClick={() => toPDF()} style={{marginLeft:5, marginRight:5}}>Download PDF</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+            style={{
+              height: 140,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column"
+            }}
+        >
+        </div>
+      </div>
+
   );
 }
 
